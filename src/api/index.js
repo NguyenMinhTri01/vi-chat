95da1ff1dfaa3b6057cdeadb7bf157eacf19111e
Nguyễn Minh Trí <17521156@ms.uit.edu.vn>
2020-10-14 09:26:08 +0700

    14_10_2020

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL : "http://localhost:5000"
+  baseURL : "https://vichat-be.herokuapp.com"
 })
-//https://vexeonline-api.herokuapp.com/api
+//https://vichat-be.herokuapp.com/
 //http://localhost:5000/api
